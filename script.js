f967f5ed0e3437f062b67fe84aac2385637058d2
Student <student@Admins-MacBook-Air.local>
2018-03-10 11:44:51 -0800

    message

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 // Declare and initialize global variables
 var canvas;
 var clearButton;
+var spread;
+var slider;
 
 function setup() {
   canvas = createCanvas(500, 400);
   canvas.parent("#canvas-area");
-  canvas.mousePressed(drawEllipse);
+  canvas.mousePressed(drawSplatter);
   clearButton = select("#clear-button");
   clearButton.mousePressed(clearScreen);
+  slider = select("#slider")
+  slider.input(updateSpread)
   background(65, 60, 88);
+  //  spread = 50;
+  updateSpread();
 }
+
+function updateSpread() {
+  spread = slider.value();
+
+}
+
+
+
 
 function drawEllipse() {
   fill("#ff0000");
@@ -19,4 +33,19 @@ function drawEllipse() {
 
 function clearScreen() {
   background(65, 60, 88);
+}
+
+function drawSplatter() {
+  fill("random((34,56,78)) ")
+  var randomNum = random(10, 15);
+  for(var i = 0; i < randomNum; i++)
+  var randomSize = random(5, 15);
+  var x = random(mouseX - spread, mouseX + spread);
+  var y = random(mouseY - spread, mouseY + spread);
+  ellipse(x, y, randomSize, randomSize);
+
+
+
+
+
 }
